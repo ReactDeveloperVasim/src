a77607950664872da00ec830d98804ce52c1c5eb
ReactDeveloperVasim <reactdeveloper.vasim@gmail.com>
2023-10-07 17:47:14 +0530

    add

--- components/GridItem.jsx
+++ components/GridItem.jsx
@@ -1,21 +1,21 @@
-import { Grid, TextField,Button } from "@mui/material";
+import { Grid, TextField, Button } from "@mui/material";
 import React from "react";
 
-export const GridItem=()=>{
-    const[data,setData]=useState();
-    return(  
+export const GridItem = () => {
+  const [data, setData] = useState();
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <TextField onChange={(e) => setData(e.target.value)} fullWidth />
+      </Grid>
+      <Grid item xs={4}>
+        <Button variant="contained" fullWidth sx={{ height: "53px" }}>
+          {" "}
+          submit
+        </Button>
+      </Grid>
 
-        <Grid container spacing={2}>
-      
-            <Grid item xs={8}>
-                <TextField  onChange={(e)=>setData(e.target.value)}  fullWidth/>
-            </Grid>
-            <Grid item xs={4}>
-                <Button variant="contained" fullWidth sx={{height:"53px"}}> submit</Button>
-            </Grid>
-           
-           
-        </Grid> 
-    );
-    
-}
+      <h1>gnjkn</h1>
+    </Grid>
+  );
+};
